6ae797d121dec43c3edc63aea535b21c171aca7a
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-01 16:48:09 +0530

    Welcome Msg Printing

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmPracticeProblemm
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+        }
+    }
+}
