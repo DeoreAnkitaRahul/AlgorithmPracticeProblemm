1b2bfa6c453966d065323dc5dc627546ebedccfb
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-02 13:12:32 +0530

    Print The Sorted List

---
+++ BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPracticeProblemm
+{
+  public class BubbleSort
+    {
+        public void DisplayNumbers()
+        {
+            int[] arr = { 87, 90, 54, 68, 40 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Algorithm Programs");
-            Anagram anagram = new Anagram();
-            anagram.DispayAnagram();
+            //Anagram anagram = new Anagram();
+            //anagram.DispayAnagram();
+            BubbleSort bubbleSort= new BubbleSort();
+            bubbleSort.DisplayNumbers();
+
         }
     }
 }
