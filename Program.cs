736b4ddb6056b394709a659369ab110c2f343e40
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-02 13:32:17 +0530

    Merge Sort Of List Of String

--- Program.cs
+++ Program.cs
@@ -9,8 +9,17 @@
             //anagram.DispayAnagram();
             //BubbleSort bubbleSort= new BubbleSort();
             //bubbleSort.DisplayNumbers();
-            InsertionSort insertionSort = new InsertionSort();
-            insertionSort.DisplayNumbers();
+            //InsertionSort insertionSort = new InsertionSort();
+            //insertionSort.DisplayNumbers();
+            int[] arr = { 8, 4, 21, 88, 45, 125, 12 };
+
+            Console.WriteLine("Array before sorting:");
+            MergeSort.PrintArray(arr);
+
+            MergeSort.mergeSort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nArray after Merge sort:");
+            MergeSort.PrintArray(arr);
 
         }
     }
