44b78888934bad14bcd6a5b885354b5e16b501af
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-02 13:00:54 +0530

    Print Two Strings Are Anagram Or Not

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Algorithm Programs");
+            Anagram anagram = new Anagram();
+            anagram.DispayAnagram();
         }
     }
 }
